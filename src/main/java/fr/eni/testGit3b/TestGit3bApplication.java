6521e28fb0b7cb97c72f09d2e73800ebfdd612e7
Stephane Gobin <sgobin@campus-eni.fr>
2022-06-09 14:09:09 +0200

    premier commit

---
+++ src/main/java/fr/eni/testGit3b/TestGit3bApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.testGit3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit3bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGit3bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/testGit3b/TestGit3bApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.testGit3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGit3bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
